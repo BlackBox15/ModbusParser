50213f9e280947472f260d1d6ddca7f1dcf2d956
BlackBox15 <mk1221@ya.ru>
2022-03-05 22:33:38 +0500

    crc16.py // adding a crc algorithm

--- crc16.py
+++ crc16.py
@@ -11,16 +11,53 @@
 
 
 def crc16(inputString):
-##  Байт-массив из Modbus-строки, неизменяемый (immutable).
-    bytesOfString = bytes.fromhex(inputString)
-
-    for b in bytesOfString:
-##        print(f"%0#x" % b)        # Old type output string representing.
-        print("{0:#x}".format(b))   # Actual type output string representing.
     
-## Существующее значение CRC хранится в последних 2-х байтах
-## Modbus-сообщения.
-    existCRC16 = bytesOfString[(len(bytesOfString) - 1)] + \
-                 bytesOfString[(len(bytesOfString) - 2)]
+    # Полином для вычисления CRC.
+    CRCPolynome_inv = 0xA001
+    # Вычисленная CRC-сумма.
+    CRCreg = 0xFFFF
+    # Байт-массив из Modbus-строки, неизменяемый (immutable).
+    dataModbus = bytes.fromhex(inputString)
 
+    # Проходим через весь массив байт за исключением последних 2-х байт.
+    for i in range(len(dataModbus) - 2):
+        # For is here.
+        #
+        print("{0:#x}".format(dataModbus[i]))   # Actual type output string representing.
+
+        # XOR'им байт с контрольной суммой.
+        CRCreg ^= dataModbus[i]
+        
+        # Побитовые операции.
+        for j in range(8):
+            # For is here
+            #
+            CRCreg >>= 1
+            
+##            if (CRCreg & 1) == 1:
+##                CRCreg >>= 1
+##                CRCreg ^= CRCPolynome_inv
+##            else:
+##                CRCreg >>= 1
+            
+
+            
+    print("0:#x".format(CRCreg))
+
+
+
+
+
+
+
+
+##    for b in bytesOfString:
+####        print(f"%0#x" % b)        # Old type output string representing.
+##        print("{0:#x}".format(b))   # Actual type output string representing.
+##    
+#### Существующее значение CRC хранится в последних 2-х байтах
+#### Modbus-сообщения.
+##    existCRC16 = bytesOfString[(len(bytesOfString) - 1)] + \
+##                 bytesOfString[(len(bytesOfString) - 2)]
+##
 crc16("01 03 43 8f b3 78")  # Just for testing.
