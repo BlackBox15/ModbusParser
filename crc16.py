f8d7758f11a2ee2f884827d8fbf49826823df134
BlackBox15 <mk1221@ya.ru>
2022-03-05 08:27:47 +0500

    crc16.py // testing new string output

--- crc16.py
+++ crc16.py
@@ -5,13 +5,22 @@
 ## пересчитываем CRC-сумму для сегмента данных и сравниваем с существующим
 ## значением CRC-суммы.
 
+##-------------------------------------------------
+## Вычисление CRC-16 контрольной суммы.
+##-------------------------------------------------
+
+
 def crc16(inputString):
-##  Байт-массив из Modbus-строки.
+##  Байт-массив из Modbus-строки, неизменяемый (immutable).
     bytesOfString = bytes.fromhex(inputString)
 
-##    for b in bytesOfString:
-##        print(b)
+    for b in bytesOfString:
+##        print(f"%0#x" % b)        # Old type output string representing.
+        print("{0:#x}".format(b))   # Actual type output string representing.
     
-##  Существующее значение CRC.
+## Существующее значение CRC хранится в последних 2-х байтах
+## Modbus-сообщения.
     existCRC16 = bytesOfString[(len(bytesOfString) - 1)] + \
                  bytesOfString[(len(bytesOfString) - 2)]
+
+crc16("01 03 43 8f b3 78")  # Just for testing.
