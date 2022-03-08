50f29912ce1b5e8231a1d3ad29855a3289a40105
BlackBox15 <mk1221@ya.ru>
2022-03-08 19:30:55 +0500

    .\ModbusParser.py // Working with command line arguments.

--- HelpPage.py
+++ HelpPage.py
@@ -1,10 +1,15 @@
 ##-------------------------------------------------
 ## Вывод help-страницы.
 ##-------------------------------------------------
-print('''
+
+### Проверка использования файла автономно или через import.
+##if __name__ == "__main__":
+    
+def printHelpPage():
+    print('''
     This is my simple Modbus parser.
     
     -h                      this help page
     -p "01 02 03 3d ff"     parsing a pool-string
     -r "01 02 03 3d ff"     parsing a request-string
-''')
+    ''')
--- ModbusParser.py
+++ ModbusParser.py
@@ -1,17 +1,47 @@
 # импорт для возможности чтения входящих аргументов
 import sys
-import crc16
+##import crc16
+import HelpPage
 ##-------------------------------------------------
-actionToPerform = 0         # Choise from user to perform some action
 modbusString = ""           # MODBUS string
 ##-------------------------------------------------
 print("\n", "." * 10, "MODBUS parser", "." * 10, "\n")
 
+if (sys.argv[1]) == '-h':
+    # выводим страницу помощи
+    HelpPage.printHelpPage()
+elif (sys.argv[1]) == '-p':
+    # расшифровываем как Pool
+    print("Decode as Pool..")
+elif (sys.argv[1]) == '-r':
+    # расшифровываем как Request
+    print("Decode as Request..")
+else:
+    # выводим сообщение, что входные данные ошибочны
+    print("Unknown arguments.")
+    print("Use -h for help.")
+      
+### Список входящих аргументов.
+##args = sys.argv[:]
+##
+####print("Количество входящих аргументов: {0}".format(len(sys.argv[])))
+##print("Количество входящих аргументов")
+##print(len(sys.argv))
+##print(sys.argv)
 
-##    Запрос Modbus-строки
-modbusString = input("Input an MODBUS string\n")
+##for i in args:
+##    print(i)
 
-print("{0:x}".format(crc16.crc16(modbusString)))
+##HelpPage.printHelpPage()
+
+
+
+    
+####    Запрос Modbus-строки
+##modbusString = input("Input an MODBUS string\n")
+##
+####  Вывод Modbus-строки
+##print("{0:x}".format(crc16.crc16(modbusString)))
 
 ##actionToPerform = int(input("Input a type of data: 1 - pool, 2 - responce\n"))
 
