b98ed59c476c6018441467495b96044064911ed6
Mikhail Kulkov <mikhail.kulkov@valmet.com>
2022-03-09 17:47:43 +0500

    .\RequestDecode.py // Adding a method for Request

--- ModbusParser.py
+++ ModbusParser.py
@@ -2,14 +2,15 @@
 import sys
 import crc16
 import HelpPage
+import RequestDecode
 ##-------------------------------------------------
 ## Тестовая строка
-## -r "01 03 d7 f3 32 78 1a"    CRC isn't correct
-## -r "01 03 d7 f3 32 99 6c"    CRC is correct
+## -r "01 03 d7 f3 32 ff 78 1a"    CRC isn't correct
+## -r "01 03 d7 f3 32 ff d9 6d"    CRC is correct
 ##-------------------------------------------------
 ##print("\n", "." * 10, "MODBUS parser", "." * 10, "\n")
 
-if (len(sys.argv) > 0):
+if (len(sys.argv) > 1):
     # Случай, когда есть аргументы в командной строке.
     if (sys.argv[1] != '-p') and (sys.argv[1] != '-r') :
         # Выводим страницу помощи.
@@ -17,22 +18,30 @@ if (len(sys.argv) > 0):
     elif (sys.argv[2] != ''):
         # У нас команда или -r, или -p.
         # Наличие Modbus-строки в аргументе командной строки.
-        # Вычисляем CRC для данной Modbus-строки без последних 2 байт.
         ModbusBytes = bytes.fromhex(sys.argv[2])
+        # Вычисляем CRC для данной Modbus-строки без последних 2 байт.
+        # Крайние 2 байта "срезаем" в отдельную переменную.
         crcExisted = ModbusBytes[-2:]
-        ModbusBytes = ModbusBytes[:len(ModbusBytes) - 2]
-        crcNEW = crc16.crc16(ModbusBytes)
-        if (crcExisted == crcNEW):
-            # sldkjflsjdflsdjflsjdf
+        # Modbus-сообщение без последних 2-х байт - это PDU.
+        # Protocol Data Unit.
+        pdu = ModbusBytes[:len(ModbusBytes) - 2]
+        # Вычисляем CRC для полученного PDU.
+        # Для сравнения.
+        crcNew = (crc16.crc16(pdu)).to_bytes(2, 'little')
+        
+        if (crcExisted == crcNew):
             print("CRC is correct.")
-            if (sys.argv[1] != '-p'):
+            
+            if (sys.argv[1] == '-p'):
                 # Дешифровка Modbus-сообщения как Pool.
                 print("Дешифровка Modbus-сообщения как Pool.")
-            elif (sys.argv[1] != '-r'):
+            elif (sys.argv[1] == '-r'):
                 # Дешифровка Modbus-сообщения как Request.
                 print("Дешифровка Modbus-сообщения как Request.")
+                RequestDecode.decode(pdu)
         else:
             print("CRC isn't correct")
+            
 else:
     # Случай, когда нет аргументов в командной строке.
     # Выводим страницу помощи.
--- RequestDecode.py
+++ RequestDecode.py
@@ -3,5 +3,26 @@
 ## PDU - это Modbus-строка без CRC-суммы.
 ##-------------------------------------------------
 
-def decodeRequest(inPDU):
+## Принимает 
+
+
+def decode(inPDU):
+##    print('Decode argument as Request.')
+##    for i in inPDU:
+##        print("{0:#x}".format(i))
     
+##    print("-" * 30)
+##    print("#\t\tByte\tDescription")
+##    print("-" * 30)
+##    print("{0:#x}\t\t1\tDevice address".format(inPDU[0]))
+##    print("{0:#x}\t\t2\tFunctional code".format(inPDU[1]))
+##    print("{0:#x} {1:#x}\t3,4\t1st register address".format(inPDU[2], inPDU[3]))
+##    print("{0:#x} {1:#x}\t5,6\tNums of register to read".format(inPDU[4], inPDU[5]))
+
+    print('''
+        {0:#x}          1   Device address
+        {1:#x}          2   Functional code
+        {2:#x}{3:x}     3,4 1st register address
+        {4:#x}{5:x}     5,6 Nums of register to read
+        '''.format(for i in inPDU: i))
+
